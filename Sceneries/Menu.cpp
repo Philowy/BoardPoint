882fae120afcf217337f5ae7d91059c6ac3d3072
Daniel Mrówczyński <danielmrow@hotmail.com>
2026-01-16 11:22:18 +0100

    Dev version

---
+++ Sceneries/Menu.cpp
@@ -0,0 +1,12 @@
+#include "Menu.hpp"
+#include "../Enums.hpp"
+#include <SFML/Graphics.hpp>
+
+
+void menuEvent(const std::optional<sf::Event>& event, Stage& gameState, sf::RenderWindow& window) {
+
+}
+
+void menuMain(sf::RenderWindow& window) {
+
+}
