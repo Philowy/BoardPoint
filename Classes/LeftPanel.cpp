afdd6ed227c4139befb3e774edccd8b9dd9fc1ea
Daniel Mrówczyński <danielmrow@hotmail.com>
2026-02-25 13:34:41 +0100

    very manual TODOLIST

---
+++ Classes/LeftPanel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Daniel on 25.02.2026.
+//
+
+#include "LeftPanel.hpp"
---
+++ Classes/LeftPanel.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Daniel on 25.02.2026.
+//
+
+#ifndef BOARDPOINT_LEFTPANEL_HPP
+#define BOARDPOINT_LEFTPANEL_HPP
+
+
+class LeftPanel {
+};
+
+
+#endif //BOARDPOINT_LEFTPANEL_HPP
